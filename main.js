da86927b16214647810d2975700900f35e4bece6
Michael Lohmeier <mjlohmeier@gmail.com>
2018-09-21 15:20:45 -0400

    new data to munch on

---
+++ main.js
@@ -0,0 +1,8 @@
+let tasks = [
+    { node: 'spells', depends: ['scour cauldron', 'write spells'] },
+    { node: 'scour cauldron', depends: ['sweep', 'dinner'] },
+    { node: 'write spells', depends: ['blood moon committee', 'dinner']},
+    { node: 'sweep', depends: []},
+    { node: 'dinner', depends: []},
+    { node: 'blood moon committee', depends: []}
+  ]
