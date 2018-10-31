8538004e75d05529cbf6b1c7e7d41425dab02e08
Michael Lohmeier <mjlohmeier@gmail.com>
2018-10-31 16:58:15 -0400

    HAPPY HALLOWEEN

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
+let orderTasks = tasks => {
+  let orderedTasks = [];
+  tasks.forEach(task => {
+    let index = orderedTasks.indexOf(task.node);
+    if (index === -1) {
+      orderedTasks.push(task.node);
+    }
+    console.log(orderedTasks);
+    //terminal logs three different arrays: 
+    //[ 'spells' ],
+    //[ 'write spells', 'scour cauldron', 'spells' ]
+    //[ 'dinner', 'sweep', 'write spells', 'scour cauldron', 'spells' ],
+    //then proceeds to log the output of line 33
+    //this happens because of the way the program loops and logs (lol's?) for every object in our "tasks" array
+    index = orderedTasks.indexOf(task.node);
+    task.depends.forEach(dependTask => { 
+      let dependTaskIndex = orderedTasks.indexOf(dependTask);
+      if (dependTaskIndex === -1) { 
+        orderedTasks.unshift(dependTask); //this is where you append to the beginning of the orderedTasks array
+      }
+      if (dependTaskIndex > index) { //this comparison works because index will always be zero
+        orderedTasks.splice(dependTaskIndex, 1); 
+        orderedTasks.unshift(dependTask);
+      }
+    })
+  })
+  return orderedTasks;
+}
+
 let tasks = [
-    { node: 'spells', depends: ['scour cauldron', 'write spells'] },
-    { node: 'scour cauldron', depends: ['sweep', 'dinner'] },
-    { node: 'write spells', depends: ['blood moon committee', 'dinner']},
+
     { node: 'sweep', depends: []},
     { node: 'dinner', depends: []},
-    { node: 'blood moon committee', depends: []}
+    { node: 'blood moon committee', depends: []},
+    { node: 'scour cauldron', depends: ['sweep', 'dinner'] },
+    { node: 'write spells', depends: ['blood moon committee', 'dinner']},
+    { node: 'cast spells', depends: ['scour cauldron', 'write spells'] }
+
   ]
 
-  //I'm new to this magic thing, how do I put this in a spell?
+console.log(orderTasks(tasks));
